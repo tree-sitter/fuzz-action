8425f133840b443ad0006f226c146720bb693369
ObserverOfTime <chronobserver@disroot.org>
2024-03-18 19:44:49 +0200

    feat: assert that the language is set

--- fuzzer.c
+++ fuzzer.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <tree_sitter/api.h>
 
 #ifndef language
@@ -6,15 +7,15 @@
 
 const TSLanguage *language(void);
 
-int LLVMFuzzerTestOneInput(const uint8_t * data, const size_t len) {
+int LLVMFuzzerTestOneInput(const uint8_t *data, const size_t len) {
     // Create a parser.
     TSParser *parser = ts_parser_new();
 
     // Set the parser's language.
-    ts_parser_set_language(parser, language());
+    assert(ts_parser_set_language(parser, language()));
 
     // Build a syntax tree based on source code stored in a string.
-    TSTree *tree = ts_parser_parse_string(parser, NULL, (const char *) data, len);
+    TSTree *tree = ts_parser_parse_string(parser, NULL, (const char *)data, len);
 
     // Free all of the heap-allocated memory.
     ts_tree_delete(tree);
